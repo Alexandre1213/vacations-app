62a9f8df34a28f62256390f928c79b4d32775a13
Alexandre <alex.apps.12@gmail.com>
2019-02-15 01:49:15 +0100

    data

--- main.js
+++ main.js
@@ -13,13 +13,14 @@ const port = process.env.PORT || 5000;
 app.get(/\/[0-9]{6}/gm, (req, res) => {
     console.log('GET');
     let id = req.path.substr(1, 6);
+    let data = '';
     connection.query('SELECT data FROM user_data WHERE u_id = ' + id, function (error, results, fields) {
         if(results[0] == undefined) { res.send('undefined'); return; }
         if (error) throw error;
-        res.status(200).send(results[0].data).end();
+        data = results[0].data;
         return;
     });
-    return;
+    return res.status(200).send(data).end();
 });
 
 app.listen(port, () => console.log(`Starting server on port ${port} !`));
