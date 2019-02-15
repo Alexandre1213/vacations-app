476c773e0cd1299f16efc4d4763b5ae165c0bd73
Alexandre <alex.apps.12@gmail.com>
2019-02-15 16:52:35 +0100

    fix

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 5000;
 app.get(/\/[0-9]{6}/gm, (req, res) => {
     console.log('GET');
     let id = req.path.substr(1, 6);
+    let data = '';
     connection.query('SELECT data FROM user_data WHERE u_id = ' + id, function (error, results, fields) {
         if(results[0] == undefined) { res.send('undefined'); return; }
         if (error) throw error;
